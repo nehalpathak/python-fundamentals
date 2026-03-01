62f5799f28dc0de1ce53ea45da842a76fba20372
nehalpathak <nehal.pathak14@gmail.com>
2026-03-01 14:22:51 +0530

    Add Part 1 Exercise 1 - hello world and print statements

---
+++ part1/01_hello_world.py
@@ -0,0 +1,6 @@
+# MOOC Part 1 - Exercise 1: Getting Started
+# Learning: print statements and basic output
+
+print("Hello World!")
+print("My name is Nehal")
+print("I am learning Python for Erasmus Mundus DEAI")
