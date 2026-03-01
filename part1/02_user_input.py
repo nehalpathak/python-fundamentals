5fef1ef11c4523d3710089e2b6fb4d9e646c3811
nehalpathak <nehal.pathak14@gmail.com>
2026-03-01 14:26:43 +0530

    Add Part 1 Exercise 2 - user input and f-strings

---
+++ part1/02_user_input.py
@@ -0,0 +1,8 @@
+# MOOC Part 1 - Exercise 2: Information from the user
+# Learning: input() function
+
+name = input("What is your name? ")
+print(f"Hello, {name}!")
+
+age = input("How old are you? ")
+print(f"You are {age} years old")
